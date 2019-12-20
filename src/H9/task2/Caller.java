822aed8101a16a9720aff680f6d9b5ee3c05f6f5
DolphieDude <andreycherevat@gmail.com>
2019-12-20 21:05:06 +0200

    task 2 finished

--- src/H9/task2/Caller.java
+++ src/H9/task2/Caller.java
@@ -2,15 +2,15 @@ package H9.task2;
 
 import java.util.concurrent.Callable;
 
-public class Caller implements Callable {
-    int name;
+public class Caller implements Callable<String> {
+    private int name;
 
-    public Caller(int name) {
+    Caller(int name) {
         this.name = name;
     }
 
     @Override
-    public Object call() throws Exception {
+    public String call() throws Exception {
         for (int i = 0; i < 30; i++) {
             System.out.println(Thread.currentThread().getName() + " name " + this.name);
             Thread.sleep(100);
--- src/H9/task2/Main.java
+++ src/H9/task2/Main.java
@@ -1,12 +1,17 @@
 package H9.task2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         ExecutorService exec = Executors.newFixedThreadPool(2);
-        Future[] futures = new Future[3];
-        for (int i = 0; i < futures.length; i++) futures[i] = exec.submit(new Caller(i));
+        List<Callable<String>> callables = new ArrayList(3);
+        for (int i = 0; i < 3; i++) callables.add(new Caller(i));
+        List<Future<String>> futures = exec.invokeAll(callables);
         for (Future f: futures) System.out.println(f.get());
+//        for (int i = 0; i < futures.size(); i++) futures.add(exec.submit(new Caller(i)));
+//        for (Future f: futures) System.out.println(f.get());
     }
 }
